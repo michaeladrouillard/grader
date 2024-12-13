427e45bc3c059ff6440c9075d005b70f7180e7bf
michaeladrouillard <droumich99@gmail.com>
2024-12-13 13:12:59 -0500

    mod render api url

--- docs/js/grader.js
+++ docs/js/grader.js
@@ -1,6 +1,6 @@
 
 
-const API_ENDPOINT = 'https://grader-a04u.onrender.com'; // Replace with your deployed API endpoint
+const API_ENDPOINT = 'https://grader-a04u.onrender.com/api/grade'; // Replace with your deployed API endpoint
 
 document.getElementById('gradeForm').addEventListener('submit', async (e) => {
     e.preventDefault();
